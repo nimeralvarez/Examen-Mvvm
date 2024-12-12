d72f6775f36733dc80b6dd706c8776494ff28f95
NimerAlvarez_T62041035 <114015379+nimeralvarez@users.noreply.github.com>
2024-12-11 18:11:32 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Examen_Mvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
